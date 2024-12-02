d0919c070567876339450e3c10b56a65e70b5aa5
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2024-12-02 12:02:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aqui podemos adicionar funcionalidades interativas mais tarde, como animações ou event listeners
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada e pronta!");
+});
